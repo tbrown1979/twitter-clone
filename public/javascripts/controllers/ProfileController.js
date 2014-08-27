dd015b2c2cf5b68bda3ea201e265822cee15365c
tbrown1979 <tbrown@uni.edu>
2014-08-27 14:33:57 -0500

    Fix ProfileController to no longer deal with promise. The promise is resolved before it reaches the controller.

--- public/javascripts/controllers/ProfileController.js
+++ public/javascripts/controllers/ProfileController.js
@@ -1,10 +1,5 @@
 angular.module('ProfileCtrl', [])
   .controller('ProfileController', ['$scope', 'userData', function($scope, userData) {
-    userData.get().success(function(data, status) {
-      console.log("GOING BIG");
-      console.log(data);
-      $scope.data = data;
-      $scope.username = data.user.username;
-      console.log($scope.username);
-    });
+    $scope.username = userData.data.user.username;
+    console.log($scope.username);
   }]);
